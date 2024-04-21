a922f430c461ecd576b0ac425690373a9a52a269
irfan juliana winandar <irfanjulianawinandar@gmail.com>
2024-04-21 22:37:00 +0700

    first init project

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>learn next.js fundamental</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
