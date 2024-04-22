1d66e9c6f5531188931cae429a7a60dfae23332e
irfan juliana winandar <irfanjulianawinandar@gmail.com>
2024-04-22 22:19:03 +0700

    penggunaan Link untuk SPA

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>learn next.js fundamental</title>
+      </head>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
+    </html>
+  );
+}
