a9ee76917ea7835cd2b442f6a50490c8338fffca
irfan juliana winandar <irfanjulianawinandar@gmail.com>
2024-04-23 20:56:54 +0700

    perpendek path dan static assets

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,9 +1,16 @@
-import Heading from "../../../components/Heading";
+import Heading from "@/components/Heading";
 
 export default function PostPage() {
   return (
     <>
       <Heading>belajar next.js</Heading>
+      <img
+        src="/images/img.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>halaman dari blog</p>
     </>
   );
--- app/blog/latihan-route-nextjs/page.jsx
+++ app/blog/latihan-route-nextjs/page.jsx
@@ -1,9 +1,16 @@
-import Heading from "../../../components/Heading";
+import Heading from "@/components/Heading";
 
 export default function PostPage() {
   return (
     <>
       <Heading>Latihan Route Nextjs</Heading>
+      <img
+        src="/images/img.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="mb-2 rounded"
+      />
       <p>halaman dari blog</p>
     </>
   );
